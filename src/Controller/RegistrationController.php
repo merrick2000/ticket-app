bc82880dc2fb63d2edcaedb16f48df2073991f34
merrick2000 <menongbemerrick@@gmail.com>
2021-07-22 17:53:13 +0100

    22-07-21

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,25 +2,75 @@
 
 namespace App\Controller;
 
+use App\Entity\MyTicketUser;
+use FOS\RestBundle\Context\Context;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\MyTicketUserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
-class RegistrationController extends AbstractController
+/**
+ * @Route("/api", name="api_")
+ */
+class RegistrationController extends AbstractFOSRestController
 {
-    private $userRepository;
-    public function __construct(MyTicketUserRepository  $userRepo)
-    {
-        $this->userRepository = $userRepo;   
-    }
     /**
-     * @Route("/register", name="registration")
+     * @var MyTicketUserRepository
      */
-    public function createUser(Request $request): Response
+    private $userRepository;
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(MyTicketUserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager)
     {
-       
+        $this->userRepository = $userRepository;
+        $this->passwordEncoder = $passwordEncoder;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/register", name="register", methods={"POST"})
+     * @param Request $request
+     * @return \FOS\RestBundle\View\View
+     */
+    public function index(Request $request)
+    {
+        $jsonReceived = $request->getContent();
+        $email = $request->get('email');
+        $password = $request->get('password');
+
+        $user = $this->userRepository->findOneBy([
+            'email' => $email,
+        ]);
+
+        if (!is_null($user)) {
+            return $this->view([
+                'message' => 'User already exists'
+            ], Response::HTTP_CONFLICT);
+        }
+
+        $user = new MyTicketUser();
+
+        $user->setEmail($email);
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $password)
+        );
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $this->view($user, Response::HTTP_CREATED)->setContext((new Context())->setGroups(['user:read']));
     }
 }
